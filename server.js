b001f0eaecf4a77c1d04f60028806b6d6ec816a5
RichDobrz <rjdobrz@gmail.com>
2019-02-25 18:26:52 -0500

    basic serever routing

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express")
-
 const PORT = process.env.PORT || 8080
-
 const app = express()
+const htmlRoutes = require("./routes/htmlRoutes")
+
+app.use(htmlRoutes)
 
 app.listen(PORT, () => {
   console.log("Server is listening on PORT http://localhost:" + PORT)
