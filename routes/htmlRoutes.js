bf168345446c9a5d487b1522979acab360713b42
RichDobrz <rjdobrz@gmail.com>
2019-02-26 14:08:04 -0500

    started input of all class CC abilities to usable object

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,4 +6,24 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
+const spellListByClass = {
+  deathknight: {
+    stun: ["Asphyxiate", "Gnaw", "Remorseless Winter"],
+    knockback: ["Gorefiend Grasp"]
+  },
+  druid: {
+    root: ["Entangling Root", "Mass Entanglement"],
+    stun: ["Main", "Mighty Bash", "Rake"],
+    incapacitate: ["Intimidating Roar"],
+    disorient: ["Cyclone"],
+    knockback: ["Typhoon", "Ursol's Vortex"]
+  },
+  hunter: {
+    root: ["Charge", "Entrapment", "Narrow Escape"],
+    stun: ["Binding Shot", "Intimidation"],
+    incapacitate: ["Freezing Trap", "Wyvern Sting"],
+
+  }
+}
+
 module.exports = router
