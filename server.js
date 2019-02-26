595d70310c4e38818390b318b913c95b87ee210d
RichDobrz <rjdobrz@gmail.com>
2019-02-25 19:13:20 -0500

    blizzard API setup

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const PORT = process.env.PORT || 8080
 const app = express()
 const htmlRoutes = require("./routes/htmlRoutes")
 
+var BnetStrategy = require('passport-bnet').Strategy;
+var BNET_ID = process.env.BNET_ID
+var BNET_SECRET = process.env.BNET_SECRET
+ 
+// Use the BnetStrategy within Passport.
+passport.use(new BnetStrategy({
+    clientID: process.env.BNET_ID,
+    clientSecret: process.env.BNET_SECRET,
+    callbackURL: "https://localhost:3000/auth/bnet/callback",
+    region: "us"
+}, function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+}));
+
 app.use(htmlRoutes)
 
 app.listen(PORT, () => {
